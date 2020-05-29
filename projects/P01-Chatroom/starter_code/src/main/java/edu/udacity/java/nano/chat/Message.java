36db69702b2ecf1e65a3e2e7f7997739d329733f
kk <sirohikrishnakant@gmail.com>
2020-05-28 19:47:27 -0500

    Chatroom application commit

--- projects/P01-Chatroom/starter_code/src/main/java/edu/udacity/java/nano/chat/Message.java
+++ projects/P01-Chatroom/starter_code/src/main/java/edu/udacity/java/nano/chat/Message.java
@@ -1,8 +1,63 @@
 package edu.udacity.java.nano.chat;
 
+import com.alibaba.fastjson.JSON;
+
 /**
  * WebSocket message model
  */
 public class Message {
-    // TODO: add message model.
+    public static final String SPEAK = "SPEAK";
+    public static final String QUIT = "QUIT";
+    public static final String ENTER = "ENTER";
+
+    private String username;
+    private String msg;
+    private Integer onlineCount;
+    private String type;
+
+    public Message(){}
+
+    public Message(String username, String type, String msg, Integer onlineCount)
+    {
+        this.username = username;
+        this.onlineCount = onlineCount;
+        this.msg = msg;
+        this.type = type;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Integer getOnlineCount() {
+        return onlineCount;
+    }
+
+    public void setOnlineCount(Integer onlineCount) {
+        this.onlineCount = onlineCount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public static String jsonStr(String type, String username, String msg, Integer onlineCount){
+        return JSON.toJSONString(new Message(username, type, msg, onlineCount));
+    }
 }
--- projects/P01-Chatroom/starter_code/src/main/java/edu/udacity/java/nano/chat/WebSocketChatServer.java
+++ projects/P01-Chatroom/starter_code/src/main/java/edu/udacity/java/nano/chat/WebSocketChatServer.java
@@ -1,9 +1,11 @@
 package edu.udacity.java.nano.chat;
 
+import com.alibaba.fastjson.JSON;
 import org.springframework.stereotype.Component;
 
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -24,15 +26,22 @@ public class WebSocketChatServer {
     private static Map<String, Session> onlineSessions = new ConcurrentHashMap<>();
 
     private static void sendMessageToAll(String msg) {
-        //TODO: add send message method.
+        onlineSessions.forEach((key, value) -> {
+                try {
+                    value.getBasicRemote().sendText(msg);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        });
     }
 
     /**
      * Open connection, 1) add session, 2) add user.
      */
     @OnOpen
-    public void onOpen(Session session) {
-        //TODO: add on open connection.
+    public void onOpen(Session session) throws IOException {
+        onlineSessions.put(session.getId(), session);
+        sendMessageToAll(Message.jsonStr(Message.ENTER, "", "", onlineSessions.size()));
     }
 
     /**
@@ -40,15 +49,17 @@ public class WebSocketChatServer {
      */
     @OnMessage
     public void onMessage(Session session, String jsonStr) {
-        //TODO: add send message.
+        Message message = JSON.parseObject(jsonStr, Message.class);
+        sendMessageToAll(Message.jsonStr(Message.SPEAK, message.getUsername(), message.getMsg(), onlineSessions.size()));
     }
 
     /**
      * Close connection, 1) remove session, 2) update user.
      */
     @OnClose
-    public void onClose(Session session) {
-        //TODO: add close connection.
+    public void onClose(Session session) throws IOException {
+        onlineSessions.remove((session.getId()));
+        sendMessageToAll(Message.jsonStr(Message.QUIT, "", "", onlineSessions.size()));
     }
 
     /**
